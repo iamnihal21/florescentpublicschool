89dd3657109f1e17085de154971cdd4d8c3a1c06
iamnihal21 <104316581+iamnihal21@users.noreply.github.com>
2025-11-28 19:31:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { TestimonialsSlider } from "@/app/components/testimonials-slider"
 import { MapsSection } from "@/app/components/maps-section"
 import { Footer } from "@/app/components/footer"
 import { ResultsSection } from "@/app/components/result-secion"
+import { DiscoverSection } from "@/app/components/textDiscoverSection"
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <Header />
       <HeroSlider />
+      <DiscoverSection />
       <ContentSection />
       <ResultsSection />
       <TestimonialsSlider />
